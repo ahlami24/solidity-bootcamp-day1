abd6a8f7b1f0cf30a248a0afdfcd40be31c7c2ac
ahlami24 <abdulaziz.afia7@gmail.com>
2026-03-31 18:21:37 +0300

    Add SimpleStorage contract with basic functions

---
+++ SimpleStorage.sol
@@ -0,0 +1,13 @@
+pragma solidity ^0.8.0;
+contract SimpleStorage {
+    uint256 storedNumber;
+    function store(uint256 _number) public {
+        storedNumber = _number;
+    }
+    function retrieve() public view returns (uint256) {
+        return storedNumber;
+    }
+    function increment() public {
+        storedNumber += 1;
+    }
+}
